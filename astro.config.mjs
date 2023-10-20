2b02476bc796cc9881dd30728f5caf37f4b2477c
kiluazen <kushalsokke@gmail.com>
2023-10-20 20:32:00 +0530

    google analytics added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,20 @@
-import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog-template.netlify.app',
-  integrations: [mdx(), svelte()],
+  integrations: [mdx(), svelte(), partytown()],
   markdown: {
     shikiConfig: {
-      theme: 'nord',
+      theme: 'nord'
     },
     remarkPlugins: ['remark-gfm', 'remark-smartypants'],
-    rehypePlugins: [
-      [
-        'rehype-external-links',
-        {
-          target: '_blank',
-        },
-      ],
-    ],
-  },
-})
+    rehypePlugins: [['rehype-external-links', {
+      target: '_blank'
+    }]]
+  }
+});
