9d6d703de5583049d2900763547752e8672624bf
kiluazen <kushalsokke@gmail.com>
2024-06-27 01:55:14 +0530

    moved to tailwind, now slowly gotta remove the old css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{astro, svelte, html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
